8535fa2fdb93a1c9ccc178529f0764edf65affe2
MUHAMMAD ALI <malirashid1994@gmail.com>
2024-05-30 12:04:44 +0000

    Update train.py

--- 02-experiment-tracking/train.py
+++ 02-experiment-tracking/train.py
@@ -1,10 +1,12 @@
 import os
 import pickle
 import click
-
+import mlflow
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("nyc-taxi-experiment")
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -22,12 +24,21 @@ def run_train(data_path: str):
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+    with mlflow.start_run():
+        mlflow.set_tag("developer","muhammad ali")
+        mlflow.log_param("train-data-path",data_path)
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        max_depth=10
+        random_state=0
+        mlflow.log_param("max_depth",max_depth)
+        mlflow.log_param("random_state",random_state)
 
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+        mlflow.log_param("min_samples_split", rf.min_samples_split)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        mlflow.log_metric("rmse",rmse)
 
 if __name__ == '__main__':
     run_train()
